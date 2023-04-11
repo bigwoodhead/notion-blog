6bc61901ae707680215b666de63f01ace4e2174e
bigwoodhead <snailnoshell@gmail.com>
2023-04-11 19:05:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Bit by Bit',
+  domain: 'blog.innovatenow.one',
+  author: '',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Data Science',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
